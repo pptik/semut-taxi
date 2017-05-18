1d50d8ce59e37b0487fa6509aed23dcf05756ef0
Hendra Permana <hynra@yahoo.com>
2017-05-18 14:29:46 +0700

    login screen

--- app/src/main/java/pptik/id/semuttaxi/LoginActivity.java
+++ app/src/main/java/pptik/id/semuttaxi/LoginActivity.java
@@ -1,15 +1,30 @@
 package pptik.id.semuttaxi;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class LoginActivity extends AppCompatActivity {
+import com.loopj.android.http.RequestParams;
+
+import pptik.id.semuttaxi.connections.httprequest.ConnectionHandler;
+import pptik.id.semuttaxi.connections.httprequest.RequestRest;
+import pptik.id.semuttaxi.setup.Constant;
+
+public class LoginActivity extends AppCompatActivity implements ConnectionHandler.IConnectionResponseHandler {
+
+    private RequestRest mRequestRest;
+    private Context mContext;
+    private ProgressDialog mProgressDialog;
+    private RequestParams mParams;
+    private String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +33,33 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        mContext = this;
+        mRequestRest = new RequestRest(mContext, this);
+        mProgressDialog = new ProgressDialog(mContext);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setMessage("Memuat ...");
     }
 
+
+    private void login(String pass, int loginType, String uniqueParam){
+        mParams = new RequestParams();
+        if(loginType == 0) mParams.put("Email", uniqueParam);
+        else mParams.put("Phonenumber", uniqueParam);
+        mParams.put("Password", pass);
+        mProgressDialog.show();
+        mRequestRest.post(Constant.REST_USER_LOGIN, mParams);
+    }
+
+
+    @Override
+    public void onSuccessRequest(String pResult, String type) {
+        mProgressDialog.dismiss();
+        Log.i(TAG, pResult);
+        switch (type){
+            case Constant.REST_USER_LOGIN:
+                break;
+            case Constant.REST_ERROR:
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/pptik/id/semuttaxi/connections/httprequest/ConnectionHandler.java
@@ -0,0 +1,38 @@
+package pptik.id.semuttaxi.connections.httprequest;
+
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.entity.StringEntity;
+
+
+
+public abstract class ConnectionHandler {
+
+
+    public interface IConnectionResponseHandler {
+        public void onSuccessRequest(String pResult, String type);
+    }
+
+    protected static AsyncHttpClient client = new AsyncHttpClient();
+    protected Context mContext;
+    protected IConnectionResponseHandler responseHandler;
+
+    public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler, AsyncHttpClient mClient) {
+        mClient.get(getAbsoluteUrl(url), params, responseHandler);
+
+
+    }
+
+    public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler, AsyncHttpClient mClient) {
+        mClient.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public void post(String url, StringEntity params, AsyncHttpResponseHandler responseHandler, AsyncHttpClient mClient, Context context) {
+        mClient.post(context, getAbsoluteUrl(url), params, "application/json", responseHandler);
+    }
+    public abstract String getAbsoluteUrl(String relativeUrl);
+}
