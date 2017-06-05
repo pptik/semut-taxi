a0bbcaf4b509871643d7b4788d107dea35d2a150
Hendra Permana <hynra@yahoo.com>
2017-06-05 13:45:18 +0700

    switch : online / offline

--- app/src/main/java/project/bsts/semut/MainActivity.java
+++ app/src/main/java/project/bsts/semut/MainActivity.java
@@ -1,16 +1,20 @@
 package project.bsts.semut;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.GridView;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 
 import java.util.ArrayList;
@@ -19,9 +23,13 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import project.bsts.semut.adapters.MainMenuAdapter;
+import project.bsts.semut.connections.rest.IConnectionResponseHandler;
+import project.bsts.semut.connections.rest.RequestRest;
 import project.bsts.semut.helper.PreferenceManager;
 import project.bsts.semut.pojo.MainMenuObject;
+import project.bsts.semut.pojo.RequestStatus;
 import project.bsts.semut.setup.Constants;
+import project.bsts.semut.ui.CommonAlerts;
 import project.bsts.semut.ui.MainDrawer;
 import project.bsts.semut.utilities.CustomDrawable;
 
@@ -35,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     private Context context;
     private final String TAG = this.getClass().getSimpleName();
     PreferenceManager mPreferenceManager;
+    private RequestRest mRest;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +56,35 @@ public class MainActivity extends AppCompatActivity {
         context = this;
 
         mPreferenceManager = new PreferenceManager(context);
+        mProgressDialog = new ProgressDialog(context);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setMessage("Memuat");
 
-        switchOnline.setChecked(mPreferenceManager.getBoolean(Constants.IS_TAXI_ONLINE));
+        mRest = new RequestRest(context, (pResult, type) -> {
+            mProgressDialog.dismiss();
+            switch (type){
+                case Constants.REST_USER_UPDATE_ONLINE_STATUS:
+                    RequestStatus requestStatus = new Gson().fromJson(pResult, RequestStatus.class);
+                    if(requestStatus.getSuccess()) {
+                        Log.i(TAG, requestStatus.getMessage());
+                    }
+                    else CommonAlerts.commonError(context, requestStatus.getMessage());
+                    break;
+                case Constants.REST_ERROR:
+                    CommonAlerts.commonError(context, Constants.MESSAGE_HTTP_ERROR);
+                    break;
+            }
+        });
+
+        mRest.updateOnlineStatus(mPreferenceManager.getInt(Constants.IS_ONLINE, 0));
+        mProgressDialog.show();
+
+        switchOnline.setChecked(mPreferenceManager.getBoolean(Constants.IS_ONLINE));
         switchOnline.setOnCheckedChangeListener((compoundButton, b) -> {
-
+            int state = (b) ? 10 : 0;
+            mPreferenceManager.save(state, Constants.IS_ONLINE);
+            mRest.updateOnlineStatus(state);
+            mProgressDialog.show();
         });
 
         new MainDrawer(context, toolbar, -1).initDrawer();
